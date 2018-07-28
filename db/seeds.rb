8f6e56813be44bafb1523b174e1d9791a3c9b955
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-07-28 15:55:06 +0800

    all routes, controllers, and models added. Working navigation between pages with links

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  flat = Flat.new(
+    name: Faker::GameOfThrones.house,
+    address: Faker::Address.full_address,
+    description: Faker::Address.community,
+    price_per_night: rand(200),
+    number_of_guests: rand(10)
+  )
+  flat.save
+end
